0565f444bfac39eee3f325781152b8fcce5efc86
Robert M Ricci <rmricci121@gmail.com>
2019-10-30 00:49:09 +0000

    Done.

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,3 +1,4 @@
+require "pry"
 def join_ingredients(src)
   # Given an Array of 2-element Arrays ( [ [food1, food2], [food3, # food4]....[foodN, foodM]]):
   #
@@ -6,38 +7,28 @@ def join_ingredients(src)
   #
   # "I love (inner array element 0) and (inner array element 1) on my pizza""
   # As such, there should be a new String for each inner array, or pair
-  ingredients = [
-    ["food1", "food2"],
-    ["food3", "food4"],
-    ["foodN", "foodM"]
-    ]
- 
-   
+  idx = 0 
+  pizza = []
+  while idx < src.count do
+  pizza.push("I love #{src[idx][0]} and #{src[idx][1]} on my pizza")
+   idx += 1
+  end
+return pizza
 end
 
 def find_greater_pair(src)
   # src will be an array of [ [number1, number2], ... [numberN, numberM] ]
   # Produce a new Array that contains the larger number of each of the pairs
   # that are in the inner Arrays
-  number_pairs =[
-    [1,4],
-    [39,8],
-    [5,7]
-    ]
-  outer_results = []
-row_index = 0
-while row_index < number_pairs.count do
-  element_index = 0
-  inner_results = []
-  while element_index < number_pairs[row_index].count do
-    if number_pairs[row_index][element_index][0] > number_pairs[row_index][element_index][1]
-      puts number_pairs[row_index][element_index][0]
-    end
-    element_index += 1
-  end
-  outer_results << inner_results
-  row_index += 1
+ 
+ row_index=0 
+ outer_results=[]
+
+while row_index < src.count do
+  outer_results.push(src[row_index].max)
+  row_index += 1 
 end
+outer_results
 end
 
 def total_even_pairs(src)
@@ -48,20 +39,14 @@ def total_even_pairs(src)
   # As a reminder any number % 2 will return 0 or 1. If the result is 0, then
   # the number was even. Review the operator documentation if you've forgotten
   # this!
-  number_pairs =[
-    [2,4],
-    [38,8],
-    [5,7]
-    ]
   
-  total = 0
-row_index = 0
-while row_index < number_pairs.count do
-  element_index = 0
-  while element_index < number_pairs[row_index].count do
-    total += number_pairs[row_index][element_index]
-    element_index += 1
-  end
-  row_index += 1
+  idx = 0
+  total = 0 
+  while idx < src.length 
+  if src[idx][0] % 2 == 0  && src[idx][1] % 2 == 0 
+     total += src[idx][0] + src[idx][1]  
+    end
+  idx += 1 
 end
+return total
 end
